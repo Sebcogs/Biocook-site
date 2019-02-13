79bafa5fd10e38c3b0297e8c8cd5f268552b7ab0
Sebastian Cogswell <47586558+Sebcogs@users.noreply.github.com>
2019-02-13 17:11:21 +0800

    Add files via upload
    add files to en folder

---
+++ en/news-en.php
@@ -0,0 +1,4 @@
+<p>
+<h4> Jan 2015:</h4><p>Receive 4.5M RMB investment to continue towards commercialization <a href="#">Read</a></p>
+<h4> Dec 2014:</h4><p>CMA certified testing agency confirms our air and water emissions are as expected. <a href="News.html#">Read</a></p>
+<h4> Aug 2014:</h4><p>HD Corp completes construction of 1000L Pilot plant in Zhuhai <a href="News.html#">Read</a></p>
